733229fdc07ad721a87ecc9d05c6fcb47d333a85
taxguide <singhparash06@gmail.com>
2026-07-20 15:19:03 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
 
+project = 'Take Home Pay Calculator'
+author = 'Take Home Pay Calculator'
+release = '1.0'
+
+extensions = [
+    'sphinx_sitemap',
+]
+
+# Templates
+templates_path = ['_templates']
+
+exclude_patterns = []
+
+html_theme = 'alabaster'
+
+# Static files
+html_static_path = ['_static']
+
+language = 'en'
+
+html_title = "Take Home Pay Calculator"
+
+# Sitemap
+html_baseurl = html_baseurl = "https://cheap-car-insurance-cheap-car-insurance.readthedocs-hosted.com/en/latest/"
+sitemap_url_scheme = "{link}"
